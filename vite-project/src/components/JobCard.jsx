c4a1c14ce9a96ce424cc12312b0aecfc6b24b8c3
iam-riyaz <riyazahmad.online@gmail.com>
2024-05-22 00:24:41 +0530

    tailwind css added and card created

--- vite-project/src/components/JobCard.jsx
+++ vite-project/src/components/JobCard.jsx
@@ -4,11 +4,23 @@ import React from 'react'
 function JobCard() {
   return (
     <div>
-      <Card sx={{minWidth:230,}}>
-           <CardContent>
-           
-           </CardContent>
-      </Card>
+      <div className='border-2 border-gray-200 shadow-xl min-w-72 p-4 min-h-[500px] rounded-xl' >
+               <div>
+                   <div className='flex justify-start gap-4 items-center'>
+                    <img className='h-12' src="https://logo.clearbit.com/dropbox.com" alt="" />
+                    <div >
+                      <p className='text-gray-400 '>Dropbox</p>
+                      <p className='text-gray-700 text-lg font-semibold'>Job Role</p>
+                      <p className='text-gray-600 text-sm'>Location</p>
+                    </div>
+                   </div>
+
+                   <div className='py-2 text-gray-500'>
+                    <span>Estimeted Salery: ₹19-22LPA ✅</span>
+                   </div>
+
+               </div>
+      </div>
     </div>
   )
 }
