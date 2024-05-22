47b025fb1eac0960e4fb59cc4895aa8936f5ab8f
flavum-riyaz <riyaz@flhealth.in>
2024-05-22 18:30:41 +0530

    job card created and show more button added

--- vite-project/src/Pages/Dashboard.jsx
+++ vite-project/src/Pages/Dashboard.jsx
@@ -24,7 +24,7 @@ function Dashboard() {
         {jobs.map((job) => {
           return (
             <>
-              <JobCard />
+              <JobCard job={job} />
             </>
           );
         })}
--- vite-project/src/components/JobCard.jsx
+++ vite-project/src/components/JobCard.jsx
@@ -1,28 +1,92 @@
-import { Card, CardContent } from '@mui/material'
-import React from 'react'
+import { Card, CardContent } from "@mui/material";
+import React, { useState } from "react";
 
-function JobCard() {
+function JobCard({ job }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const {
+    companyName,
+    logoUrl,
+    jobDetailsFromCompany,
+    location,
+    maxJdSalary,
+    minJdSalary,
+    minExp,
+    jobRole,
+    salaryCurrencyCode,
+  } = job;
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const maxLength = 250;
+  const displayDetailText = isExpanded
+    ? jobDetailsFromCompany
+    : `${jobDetailsFromCompany.substring(0, maxLength)}${
+        jobDetailsFromCompany.length > maxLength ? "..." : ""
+      }`;
+
   return (
     <div>
-      <div className='border-2 border-gray-200 shadow-xl min-w-72 p-4 min-h-[500px] rounded-xl' >
-               <div>
-                   <div className='flex justify-start gap-4 items-center'>
-                    <img className='h-12' src="https://logo.clearbit.com/dropbox.com" alt="" />
-                    <div >
-                      <p className='text-gray-400 '>Dropbox</p>
-                      <p className='text-gray-700 text-lg font-semibold'>Job Role</p>
-                      <p className='text-gray-600 text-sm'>Location</p>
-                    </div>
-                   </div>
+      <div className="border-2 border-gray-200 shadow-xl w-72 p-4 min-h-[500px] rounded-xl">
+        <div>
+          <div className="flex justify-start gap-4 items-center">
+            <img className="max-h-12 max-w-12" src={logoUrl} alt="" />
+            <div>
+              <p className="text-gray-400 ">{companyName}</p>
+              <p className="text-gray-700 text-lg font-semibold">{jobRole}</p>
+              <p className="text-gray-600 text-sm">{location}</p>
+            </div>
+          </div>
 
-                   <div className='py-2 text-gray-500'>
-                    <span>Estimeted Salery: ₹19-22LPA ✅</span>
-                   </div>
+          <div className="py-2 text-gray-500">
+            <span className="font-semibold text-sm">
+              Estimeted Salery:{" "}
+              <span className="text-yellow-600">
+                {" "}
+                {salaryCurrencyCode} {minJdSalary ? `${minJdSalary}-` : null}
+                {maxJdSalary} LPA ✅
+              </span>
+            </span>
+          </div>
 
-               </div>
+          <div>
+            <p className="font-semibold pt-1">About Company:</p>
+            <p className="text-sm font-semibold text-gray-700">About Us</p>
+            <div className="">
+              <div className="">
+                <div className={isExpanded?"h-[200px] overflow-y-scroll my-2":" h-[200px] my-2"}>
+                {
+                  <p className="-z-10">
+                    {displayDetailText}{" "}
+                    {" "}
+                  </p>
+                }
+                </div>
+                <button
+                      onClick={toggleExpand}
+                      className=" rounded-xl text-sm bg-green-200 px-2 pb-1"
+                    >
+                      {isExpanded ? "Show Less" : "Show More"}
+                    </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-2">
+            <p className="text-blue-400 font-semibold">Minimum Experience</p>
+            {minExp ? <p>{minExp} Years</p> : "Any number of years"}
+          </div>
+          <div>
+            <button className="w-full py-2 bg-teal-400 rounded-xl font-bold">
+              Easy Apply
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default JobCard
+export default JobCard;
